5debaff371aa9bd724a7d631bd341af53cb1f72a
JayaDhakar <jayadhakar.raj.csrl@gmail.com>
2025-04-09 12:13:57 +0530

    updated code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    CommentsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://Thread-user:Thread_user@cluster0free.wohl103.mongodb.net/threads?retryWrites=true&w=majority&appName=Cluster0Free',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
